10573f6ed6489b8ca508442657f009965bb49adf
Jasdeep Singh <narang.jasdeep@gmail.com>
2015-06-08 15:51:06 -0400

    solution to 2-3

--- chapter2/geom.ex
+++ chapter2/geom.ex
@@ -1,5 +1,17 @@
 defmodule Geom do
 
+  @moduledoc """
+    Functions for calculating area of a given Geometric shape
+
+    Source: http://chimera.labs.oreilly.com/books/1234000001642/ch02.html#
+  """
+
+  @doc """
+    Calculates the area of a rectangle, given the length and width.
+    Returns the product of its arguments. The default value for
+    both arguments is 1.
+  """
+
   def area(x \\ 1, y \\ 1) do
     x*y
   end
